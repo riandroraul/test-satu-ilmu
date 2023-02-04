e4f7b62d20fbf9573884a2128fc91248912cd9c0
riandroraul <riandroraul@gmail.com>
2023-02-04 09:56:18 +0700

    update

--- test-frontend/src/components/Navbar.jsx
+++ test-frontend/src/components/Navbar.jsx
@@ -5,10 +5,10 @@ const Navbar = () => {
   return (
     <div className="container">
       <nav>
-        <a href="/html/">brand</a>
-        <a href="/css/">CSS</a>
-        <a href="/js/">JavaScript</a>
-        <a href="/python/">Python</a>
+        <a href="/sd/">brand</a>
+        <a href="/ads/">CSS</a>
+        <a href="/ads/">JavaScript</a>
+        <a href="/asd/">Python</a>
         <button>Sign up</button>
         <button>Login</button>
       </nav>
